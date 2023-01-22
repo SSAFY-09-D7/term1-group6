df0586c719c6939668f4af710928bbbcd188f4a7
mxnzx <i0692631@naver.com>
2023-01-22 11:47:09 +0900

    feat: [문제21] 소금쟁이 중첩 풀이 완료

--- minzi/solution/WaterStrider01.java
+++ minzi/solution/WaterStrider01.java
@@ -4,10 +4,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+//import java.io.FileInputStream;
 
 public class WaterStrider01 {
 
     public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
