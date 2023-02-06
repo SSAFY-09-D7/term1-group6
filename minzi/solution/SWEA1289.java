da2ca2ec5c2b18445a8b975139bfea9b954da2f4
mxnzx <i0692631@naver.com>
2023-02-06 23:58:14 +0900

    [SWEA] 1289. 원재의 메모리 복구하기 풀이 완료 - 재줌

--- minzi/solution/SWEA1289.java
+++ minzi/solution/SWEA1289.java
@@ -1,7 +1,8 @@
 /*
+SWEA1289. 원재의 메모리 복구하기 - 재귀
 막힌 부분
 1. init[] 초기화는 char형이므로 null임. 0으로 착각함.
-2. 재귀함수에서 리턴 전까지는 count맞다가 return count 할때 main에 0을 달고 나옴. ㅡㅡ
+2. 재귀함수에서 리턴 전까지는 count맞다가 return count 할때 main에 0을 달고 나옴. 호출했던 곳들의 변수의 값과 독립적이기때문에 당연함. =>static으로 처리해줌.
  */
 
 import java.io.BufferedReader;
@@ -10,7 +11,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class SWEA1289 {
-
+    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
@@ -23,13 +24,11 @@ public class SWEA1289 {
 
             //초기값 배열
             char[] init = new char[og.length];
-            for (int i = 0; i < init.length; i++) {
-                init[i]='0';
-            }
+            Arrays.fill(init, '0');
 
             int count = changeBit(og, init, 0,0);
 
-            sb.append("# ").append(tc).append(" ").append(count).append("\n");
+            sb.append("#").append(tc).append(" ").append(count).append("\n");
 
         }
         System.out.println(sb);
@@ -39,25 +38,26 @@ public class SWEA1289 {
     //count 바뀐 횟수
     private static int changeBit(char[] og, char[] init, int idx, int count) {
         //========== basis part =========
-        if(Arrays.toString(og).equals(Arrays.toString(init))) {
-//            System.out.println(count);
 
-            return count;   //여기서 리턴하면 0으로 나감
+        if (Arrays.toString(og).equals(Arrays.toString(init))) {
+            ans = count;
+            return ans;
+            //return count;   //여기서 리턴하면 0으로 나감. 왜? -> 호출했던곳들로 가다보면 거기 ans 0임. -> static으로 선언해주자
         }
         //========== inductive part =========
 
         //둘이 다르면 temp 바꾼다
-        if(og[idx] != init[idx]) {
+        if (og[idx] != init[idx]) {
             char c = og[idx];
             for (int i = idx; i < init.length; i++) {
                 init[i] = c;
             }
-            System.out.println(Arrays.toString(init));
-            changeBit(og,init,idx+1,count+1);
+            changeBit(og, init, idx + 1, count + 1);
         } else {
-            changeBit(og,init, idx+1,count);
+            //아니면 idx만 올려
+            changeBit(og, init, idx + 1, count);
         }
-        return count;
+        return ans;
 
     }
 }
