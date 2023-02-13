bce545e4fb7bcc6e03a717a3d615d263e3719c5d
김선진 <tjswls8062@skuniv.ac.kr>
2023-02-13 13:43:19 +0900

    주차장 구현 완료

--- Sunjin/주차장.java
+++ Sunjin/주차장.java
@@ -1,73 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main2 {
+public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
+        StringTokenizer st ;
+
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
-        HashMap<Integer, Integer> fee = new HashMap<>();
-        for(int i = 1; i <= N; i++) {
-        	int rs = Integer.parseInt(br.readLine());
-        	fee.put(i, rs);
+
+        int[][] arr = new int[N + 1][2];
+        int[]cars = new int[M + 1];
+        int parkCount = 0;
+        int money = 0;
+
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
         }
-        
-        HashMap<Integer, Integer> weight = new HashMap<>();
-        for(int i = 1; i <= M; i++) {
-        	int wk = Integer.parseInt(br.readLine());
-        	weight.put(i, wk);
+
+        for(int i=1; i<=M; i++) {
+            st = new StringTokenizer(br.readLine());
+            cars[i] = Integer.parseInt(st.nextToken());
         }
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i <= 2 * M; i++) {
-        	int order = Integer.parseInt(br.readLine());
-        	q.add(order);
-        }
-        
-        HashMap<Integer, Integer> visited = new HashMap<>();
-        for(int i = 1; i <= N; i++) {
-        	visited.put(i, null);
-        }
-        
-        // 대기 큐
         Queue<Integer> waitQ = new LinkedList<>();
-        int sum = 0;
-        while(!q.isEmpty()) {
-        	int value = q.poll();
-        	// 들어가는 경우
-        	L :if(value > 0) {
-        		for(int key : visited.keySet()) {
-        			if(visited.get(key) == null) {
-        				visited.put(key, waitQ.poll());
-        				break L;
-        			}
-        		}
-        		waitQ.add(value);
-        	}
-        	// 나가는 경우
-        	if(value < 0) {
-        		for(int key : visited.keySet()) {
-        			if(visited.get(key) == Integer.valueOf(-value)) {
-        				sum += fee.get(Integer.valueOf(key)) * weight.get(Integer.valueOf(-value));
-        				if(waitQ.size() == 0) {
-        					visited.put(key, null);
-        					break;
-        				}
-        				visited.put(key, waitQ.poll());
-        				break;
-        			}
-        		}
-        	}
+        int num;
+        for(int i=0; i<2*M; i++) {
+            num = Integer.parseInt(br.readLine());  
+
+            if(num>0) {
+                if(parkCount == N) {
+                    waitQ.add(num);
+                }
+                else {
+                    for(int j=1; j<=N; j++) {
+                        if(arr[j][1] == 0) {
+                            arr[j][1] = num;
+                            parkCount += 1;
+                            money += arr[j][0] * cars[num];
+                            break;
+                        }
+                    }
+                }
+            }
+            else {    
+                for(int j=1; j<=N; j++) {
+                    if(arr[j][1] == -num) {
+                        if(waitQ.isEmpty()) {
+                            arr[j][1] = 0;
+                            parkCount -= 1;
+                            break;
+                        }
+                        else {
+                            int newC = waitQ.remove();
+                            arr[j][1] = newC;
+                            money += arr[j][0] * cars[newC];
+                        }
+                    }
+                }
+            }
         }
-        System.out.println(sum);
+        System.out.println(money);
     }
 }
