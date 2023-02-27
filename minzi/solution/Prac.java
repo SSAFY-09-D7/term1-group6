c1e49c7fb99bf23f7a8bb895da7444aa70db8b34
mxnzx <i0692631@naver.com>
2023-02-27 23:13:33 +0900

    BOJ]3184. 양 풀이 완료 - BFS

---
+++ minzi/solution/Prac.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Prac {
+    public static void main(String[] args) {
+
+        char[] og = {'1','0'};
+        char[] init = {'1','0'};
+        System.out.println(Arrays.toString(og).equals(Arrays.toString(init)));
+    }
+}
--- minzi/solution/Problem5.java
+++ minzi/solution/Problem5.java
@@ -1,4 +1,5 @@
-
+//월말평가 문제 5 - 비밀교환일기
+//[BOJ]2149 암호해독과 유사
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -8,9 +9,9 @@ public class Problem5 {
     //static char[] c = {'A','B','C','D','E','F','G','H'};
     //static String[] num = new String[]{"000000", "001111", "010011", "011100", "100110", "101001", "110101", "111010"};
 
-    // 힌글자 다른 거 처리
-//    static String replaceSim() {
-//        return ;
+    // 힌글자 다른 거 처리 return alphabet
+//    static String checkMore() {
+//
 //    }
 
     //해당 글자가 있으면 ans배열에 하나씩 추가한다. 없을 시 해당 idx를 리턴.
@@ -45,7 +46,13 @@ public class Problem5 {
                     ans[i] = 'H';
                     break;
                 default:
-                    return String.valueOf(i + 1);
+                    //없을 때 한문자열씩 비교하는 함수 호출 - 하나씩?
+                    //하나씩 돌면서 맞으면 cnt 올림 cnt>=5개 이상이면 그 알파벳 출력 후 break
+//                    char c = checkMore();
+//                    if(c값이 있으면 )
+//                        return c;
+//                    else
+//                        return String.valueOf(i + 1);
 
             }
         }
